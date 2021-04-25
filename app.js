1a9da351f0968585f0cb9c5566814ad027887c55
Bashar-Syouri <beshoo_1993@hotmail.com>
2021-04-25 14:39:01 +0300

    edit

---
+++ app.js
@@ -0,0 +1,13 @@
+let userName = prompt('what\'s your name');
+console.log(userName);
+
+let color = prompt('what\'s your fav color');
+console.log(color);
+
+let age = prompt('what\'s your age');
+console.log(age);
+
+let timeNow = prompt('what\'s the time now');
+console.log(timeNow)
+
+alert('Heloo ' + userName + ' your fav color is ' + ' and your age is ' + age);
